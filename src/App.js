cb08e6657a1861fae38c204514a1ff2ac745c040
ReaganS94 <reagansasan@gmail.com>
2023-06-13 14:51:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Upload from "./components/Upload";
+import ImageCarousel from "./components/ImageCarousel";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
 
 function App() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Upload flag={toggle} setFlag={setToggle} />
+      <ImageCarousel flag={toggle} />
     </div>
   );
 }
